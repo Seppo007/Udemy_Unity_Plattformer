6b12cb7cda55eee8a69615f4692b02ca12437712
Sebastian Schalow <schalowseb@gmail.com>
2018-06-03 18:15:45 +0200

    Remove DataPrivacy module. Make the player rotate

--- Assets/Game/Scripts/Player.cs
+++ Assets/Game/Scripts/Player.cs
@@ -5,9 +5,28 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed = 0.05f;
+    public GameObject model;
+
+    private float targetYRotation;
 
     // Update is called once per frame
     private void Update() {
-        transform.position += speed * transform.forward * Input.GetAxis("Horizontal");
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        MovePlayerHorizontal(horizontalAxis);
+        RotatePlayerToMovementDirection(horizontalAxis);
+    }
+
+    private void MovePlayerHorizontal(float horizontalAxis) {
+        transform.position += speed * transform.forward * horizontalAxis;
+    }
+
+    private void RotatePlayerToMovementDirection(float horizontalAxis) {
+        if (horizontalAxis > 0f) {
+            targetYRotation = 0f;
+        } else if (horizontalAxis < 0f) {
+            targetYRotation = -180f;
+        }
+
+        model.transform.rotation = Quaternion.Lerp(model.transform.rotation, Quaternion.Euler(0f, targetYRotation, 0f), Time.deltaTime * 10f);
     }
 }
