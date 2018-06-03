2faa9f3cb8f6b96ca23776c34620ae0d5f0aacf7
Sebastian Schalow <schalowseb@gmail.com>
2018-06-03 17:20:35 +0200

    Add the player. Add script to move the player

---
+++ Assets/Game/Scripts/Player.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 0.05f;
+
+    // Update is called once per frame
+    private void Update() {
+        transform.position += speed * transform.forward * Input.GetAxis("Horizontal");
+    }
+}
