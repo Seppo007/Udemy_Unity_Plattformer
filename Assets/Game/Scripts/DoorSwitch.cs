f9aea0226ce8050197323668e468f989e06942cc
Sebastian Schalow <schalowseb@gmail.com>
2018-06-06 22:29:42 +0200

    Dynamically switch the console signal material. Track only first input of Fire1 when pressed

--- Assets/Game/Scripts/DoorSwitch.cs
+++ Assets/Game/Scripts/DoorSwitch.cs
@@ -6,16 +6,20 @@ using UnityEngine;
 public class DoorSwitch : MonoBehaviour {
 
     public Animator doorAnimator;
-
-
+    public MeshRenderer meshRenderer;
 
     private void OnTriggerStay(Collider other) {
-        if (Input.GetAxisRaw("Fire1") != 0f) {
+        if (Input.GetAxisRaw("Fire1") != 0f && !doorAnimator.GetBool("isOpen")) {
             OpenDoor();
         }
     }
 
     private void OpenDoor() {
         doorAnimator.SetBool("isOpen", true);
+        Material[] mats = meshRenderer.materials;
+        Material lightBulbOff = mats[2];
+        mats[2] = mats[1];
+        mats[1] = lightBulbOff;
+        meshRenderer.materials = mats;
     }
 }
