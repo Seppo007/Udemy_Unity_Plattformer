85195fc047154df5df95b0914a248a0be89891ee
Sebastian Schalow <schalowseb@gmail.com>
2018-06-06 22:11:56 +0200

    Add trigger for door interaction

---
+++ Assets/Game/Scripts/DoorSwitch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorSwitch : MonoBehaviour {
+
+    public Animator doorAnimator;
+
+
+
+    private void OnTriggerStay(Collider other) {
+        if (Input.GetAxisRaw("Fire1") != 0f) {
+            OpenDoor();
+        }
+    }
+
+    private void OpenDoor() {
+        doorAnimator.SetBool("isOpen", true);
+    }
+}
