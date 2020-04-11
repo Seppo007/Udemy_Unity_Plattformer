46c795fdad93863099ea16c03112e2fb244597bb
Sebastian Schalow <schalowseb@gmail.com>
2020-04-11 19:45:44 +0200

    Add savegame point 1 and 2

---
+++ Assets/Game/Scripts/SaveGameTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Auslöser für automatischen Speicherpunkt.
+/// </summary>
+public class SaveGameTrigger : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+    }
+
+}
