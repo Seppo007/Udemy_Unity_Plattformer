22e605d3eff1f41c82ec4d763941a59219d9b55c
Sebastian Schalow <schalowseb@gmail.com>
2020-04-11 21:35:55 +0200

    Open the door by player interaction

---
+++ Assets/Game/Scripts/DoorSwitch.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorSwitch : MonoBehaviour {
+    public Animator animator;
+
+    private void OnTriggerStay(Collider other) {
+        if (Input.GetAxisRaw("Fire1") != 0f) {
+            openTheDoor();
+        }
+    }
+
+    private void openTheDoor() {
+        animator.SetBool("isOpen", true);
+    }
+}
