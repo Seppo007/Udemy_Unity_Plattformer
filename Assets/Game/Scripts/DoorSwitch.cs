ef02162fc789e5ffa4c7cbe3e0158574f9d6f021
Sebastian Schalow <schalowseb@gmail.com>
2020-04-12 16:03:36 +0200

    Enable light switching on doorSwitch when opening the door

--- Assets/Game/Scripts/DoorSwitch.cs
+++ Assets/Game/Scripts/DoorSwitch.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DoorSwitch : MonoBehaviour {
-    public Animator animator;
+    public Animator doorAnimator;
+    public MeshRenderer lightBulbMesh;
 
     private void OnTriggerStay(Collider other) {
-        if (Input.GetAxisRaw("Fire1") != 0f) {
+        if (Input.GetAxisRaw("Fire1") != 0f && !doorAnimator.GetBool("isOpen")) {
             openTheDoor();
         }
     }
 
     private void openTheDoor() {
-        animator.SetBool("isOpen", true);
+        doorAnimator.SetBool("isOpen", true);
+        lightBulbMesh.materials = switchLights(lightBulbMesh.materials);
+    }
+
+    private Material[] switchLights(Material[] oldMaterials) {
+        Material[] switchedMaterials = lightBulbMesh.materials;
+        Material lightOn = switchedMaterials[1];
+        Material lightOff = switchedMaterials[2];
+
+        switchedMaterials[1] = lightOff;
+        switchedMaterials[2] = lightOn;
+
+        return switchedMaterials;
     }
 }
