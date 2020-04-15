dea5e9e4112092f003946b77580c603f3fc69f01
Sebastian Schalow <schalowseb@gmail.com>
2020-04-15 22:37:12 +0200

    Modify Levelmanager and scenes to switch between scene1 and scene2

---
+++ Assets/Game/Scripts/GoToScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToScene : MonoBehaviour {
+    public string scene = "";
+
+    private void OnTriggerEnter(Collider other) {
+        LevelManager levelManager = FindObjectOfType<LevelManager>();
+        levelManager.loadScene(scene);
+    }
+}
