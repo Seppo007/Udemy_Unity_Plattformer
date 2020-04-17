b1e877a0c8e07adc6c490d2c57d6901accf9f88e
Sebastian Schalow <schalowseb@gmail.com>
2020-04-17 12:44:38 +0200

    Set menu enabled default to false and enable selection in menu via keyboard

--- Assets/Game/Scripts/Menu.cs
+++ Assets/Game/Scripts/Menu.cs
@@ -2,17 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Menu : MonoBehaviour
-{
+public class Menu : MonoBehaviour {
+    private bool keyWasPressed = false;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        GetComponent<Canvas>().enabled = false;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        if (Input.GetAxisRaw("Menu") > 0f) {
+            if (!keyWasPressed) {
+                Canvas canvas = GetComponent<Canvas>();
+                canvas.enabled = !canvas.enabled;
+            }
+            keyWasPressed = true;
+        }
+        else {
+            keyWasPressed = false;
+        }
     }
 }
